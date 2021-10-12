0b1c9df92744519f26e402f229f6212f4be7ccdd
babern2303 <babern04@mail.ru>
2021-10-12 18:48:03 +0300

    [FEATURE] Hello made and random

--- project1/project1/project1.cpp
+++ project1/project1/project1.cpp
@@ -1,6 +1,18 @@
 ﻿#include <iostream>
+#include<ctime>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	std::cout << "Hello World!\n";
+	int a, b, c, random;
+	setlocale(LC_ALL, "RUS");
+	srand(time(0));
+	random = rand() % (999 - 100 + 1) + 100;
+	cout << random << endl;
+	a = random / 100;
+	b = (random / 10) % 10;
+	c = random % 10;
+	cout << "Число в обратном порядке:" << c << b << a << endl;
+    
 }
